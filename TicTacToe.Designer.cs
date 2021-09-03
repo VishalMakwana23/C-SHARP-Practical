b0ee576f9b34f96bf82b44588064eab093226417
Vishal Makwana <70998696+VishalMakwana23@users.noreply.github.com>
2021-09-03 10:52:46 +0530

    Add files via upload

---
+++ TicTacToe.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace assignment1
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playbox = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtsize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playbox
+            // 
+            this.playbox.BackColor = System.Drawing.Color.White;
+            this.playbox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.playbox.Location = new System.Drawing.Point(0, 48);
+            this.playbox.Name = "playbox";
+            this.playbox.Size = new System.Drawing.Size(668, 522);
+            this.playbox.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 47);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "make it";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtsize
+            // 
+            this.txtsize.Location = new System.Drawing.Point(163, 14);
+            this.txtsize.Name = "txtsize";
+            this.txtsize.Size = new System.Drawing.Size(46, 22);
+            this.txtsize.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "enter size of game box";
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 570);
+            this.Controls.Add(this.playbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtsize);
+            this.Controls.Add(this.label1);
+            this.Name = "TicTacToe";
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtsize;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ TicTacToe.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment1
+{
+    public partial class TicTacToe : Form
+    {
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+        int move;
+        Button[,] btns;
+        int size;
+        Boolean endgame;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtsize.Text != "")
+            {
+                button1.Text = "Re Match";
+                playbox.Controls.Clear();
+                move = 0;
+                endgame = false;
+                int locx = 0;
+                int locy = 0;
+                size = Int32.Parse(txtsize.Text);
+                btns = new Button[size, size];
+                for (int i = 0; i < size; i++)
+                {
+                    locy = locy + 50;
+                    for (int j = 0; j < size; j++)
+                    {
+                        locx = locx + 50;
+                        btns[i, j] = new Button();
+                        btns[i, j].Width = 50;
+                        btns[i, j].Height = 50;
+                        btns[i, j].Location = new Point(locx, locy);
+                        playbox.Controls.Add(btns[i, j]);
+                        btns[i, j].Click += new EventHandler(checkrun);
+                    }
+                    locx = 0;
+                }
+            }
+        }
+        private void checkrun(object sender, EventArgs e)
+        {
+            if ((sender as Button).Text == "" && endgame == false)
+            {
+                if (move % 2 == 0)
+                {
+                    (sender as Button).Text = "X";
+                }
+                else
+                {
+                    (sender as Button).Text = "O";
+                }
+                move++;
+                int ocount = 0;
+                int xcount = 0;
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (btns[i, j].Text == "O")
+                        {
+                            ocount++;
+                        }
+                        if (btns[i, j].Text == "X")
+                        {
+                            xcount++;
+                        }
+                    }
+                    if (ocount == size)
+                    {
+                        MessageBox.Show("O Wins !");
+                        endgame = true;
+                        return;
+                    }
+                    if (xcount == size)
+                    {
+                        MessageBox.Show("X Wins !");
+                        endgame = true;
+                        return;
+                    }
+                    ocount = 0;
+                    xcount = 0;
+                }
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (btns[j, i].Text == "O")
+                        {
+                            ocount++;
+                        }
+                        if (btns[j, i].Text == "X")
+                        {
+                            xcount++;
+                        }
+                    }
+                    if (ocount == size)
+                    {
+                        MessageBox.Show("O Wins !");
+                        endgame = true;
+                        return;
+                    }
+                    if (xcount == size)
+                    {
+                        MessageBox.Show("X Wins !");
+                        endgame = true;
+                        return;
+                    }
+                    ocount = 0;
+                    xcount = 0;
+                }
+                for (int i = 0; i < size; i++)
+                {
+                    if (btns[i, i].Text == "O")
+                    {
+                        ocount++;
+                    }
+                    if (btns[i, i].Text == "X")
+                    {
+                        xcount++;
+                    }
+                }
+                if (ocount == size)
+                {
+                    MessageBox.Show("O Wins !");
+                    endgame = true;
+                    return;
+                }
+                if (xcount == size)
+                {
+                    MessageBox.Show("X Wins !");
+                    endgame = true;
+                    return;
+                }
+                ocount = 0;
+                xcount = 0;
+                int jk = size-1;
+                for (int i = 0; i < size; i++)
+                {
+                    if (btns[i, jk].Text == "O")
+                    {
+                        ocount++;
+                    }
+                    if (btns[i, jk].Text == "X")
+                    {
+                        xcount++;
+                    }
+                    jk--;
+                }
+                if (ocount == size)
+                {
+                    MessageBox.Show("O Wins !");
+                    endgame = true;
+                    return;
+                }
+                if (xcount == size)
+                {
+                    MessageBox.Show("X Wins !");
+                    endgame = true;
+                    return;
+                }
+                if (move == (size * size))
+                {
+                    MessageBox.Show("Match Draw !");
+                }
+            }
+        }
+    }
+}
