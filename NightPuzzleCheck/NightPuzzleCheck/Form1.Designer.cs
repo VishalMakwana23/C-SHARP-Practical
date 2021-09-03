28330377e93f0b3be141baecc6ee6dec1e111346
Vishal Makwana <70998696+VishalMakwana23@users.noreply.github.com>
2021-09-03 10:16:12 +0530

    Add files via upload

---
+++ NightPuzzleCheck/NightPuzzleCheck/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace NightPuzzleCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playbox = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtsize = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playbox
+            // 
+            this.playbox.Location = new System.Drawing.Point(39, 70);
+            this.playbox.Name = "playbox";
+            this.playbox.Size = new System.Drawing.Size(536, 284);
+            this.playbox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "EnterSize";
+            // 
+            // txtsize
+            // 
+            this.txtsize.Location = new System.Drawing.Point(139, 25);
+            this.txtsize.Name = "txtsize";
+            this.txtsize.Size = new System.Drawing.Size(100, 20);
+            this.txtsize.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 434);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtsize);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.playbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtsize;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ NightPuzzleCheck/NightPuzzleCheck/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NightPuzzleCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Button[,] btns;
+        int size;
+        int blankx, blanky, blanktag;
+        Boolean endgame;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txtsize.Text != "" && Int32.Parse(txtsize.Text) > 2)
+            {
+                size = Int32.Parse(txtsize.Text);
+
+                int[] cn = new int[size * size];
+
+                Random rand = new Random();
+                for (int i = 0; i < (size * size); i++)
+                {
+                    int a = rand.Next(1, (size * size) + 1);
+
+                    if(i == 0)
+                    {
+                        cn[i] = a;
+                    }
+                    else
+                    {
+                        bool flage = true;
+
+                        while(flage)
+                        {
+                            int chk = 0;
+
+                            foreach(int c in cn)
+                            {
+                                if(c == a)
+                                {
+                                    a = rand.Next(1, (size * size) + 1);
+                                    chk = 1;
+                                }
+                            }
+
+                            if(chk == 0)
+                            {
+                                flage = false;
+                                cn[i] = a;
+                            }
+                        }
+                    }
+                }
+
+
+                button1.Text = "RE PLAY";
+                playbox.Controls.Clear();
+                endgame = false;
+                int colx = 0;
+                int coly = 0;
+
+                 btns = new Button[size, size];
+
+                int k = 0;
+
+                for (int i = 0; i < size; i++)
+                {
+                    coly = coly + 50;
+                    for (int j = 0; j < size; j++)
+                    {
+                        colx = colx + 50;
+
+                        btns[i, j] = new Button();
+                        btns[i, j].Width = 50;
+                        btns[i, j].Height = 50;
+                        btns[i, j].Name = (k+1).ToString();
+                        btns[i, j].Tag = k + 1;
+
+                        btns[i, j].Location = new Point(colx, coly);
+
+                        if (cn[k] == size * size)
+                        {
+                            btns[i, j].Text = "";
+                            blankx = i;
+                            blanky = j;
+                            blanktag = k + 1;
+                        }
+                        else
+                        {
+                            btns[i, j].Text = cn[k].ToString(); ;
+                        }
+
+                        playbox.Controls.Add(btns[i, j]);
+                        btns[i, j].Click += new System.EventHandler(checkRun);
+                        k++;
+
+                    }
+                    colx = 0;
+                }
+
+                
+
+            }
+        }
+        private void checkRun(object sender, EventArgs e)
+        {
+            if (endgame == false)
+            {
+
+                int x = 0, y = 0, tag = 0;
+
+                int btnadd = Int32.Parse((sender as Button).Name);
+
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if(btns[i,j] == (sender as Button))
+                        {
+                            x = i;
+                            y = j;
+                            tag = btnadd;
+                        }
+                    }
+                }
+
+                if(btnadd - size == blanktag||btnadd + size == blanktag||btnadd - 1 == blanktag && blankx == x || btnadd + 1 == blanktag && blankx == x)
+                {
+                    btns[blankx, blanky].Text = (sender as Button).Text;
+                    (sender as Button).Text = "";
+                    blankx = x;
+                    blanky = y;
+                    blanktag = tag;
+
+                    int cnt = 0;
+
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            if(btns[i,j].Text == btns[i, j].Name)
+                            {
+                                cnt++;
+                            }
+                            if(btns[size -1,size -1] == btns[i,j] && btns[i,j].Text == "")
+                            {
+                                cnt++;
+                            }
+
+                           
+                        }
+                    }
+
+                    if (cnt == (size * size))
+                    {
+                        MessageBox.Show("win");
+                        endgame = true;
+                        return;
+                    }
+                }
+            }
+
+        }
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
