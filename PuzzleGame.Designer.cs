fa1233e4e07765038842fe410c61b58b76310688
Vishal Makwana <70998696+VishalMakwana23@users.noreply.github.com>
2021-09-03 10:53:03 +0530

    Add files via upload

---
+++ PuzzleGame.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace assignment1
+{
+    partial class PuzzleGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playbox = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtsize = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playbox
+            // 
+            this.playbox.BackColor = System.Drawing.Color.White;
+            this.playbox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.playbox.Location = new System.Drawing.Point(0, 43);
+            this.playbox.Name = "playbox";
+            this.playbox.Size = new System.Drawing.Size(670, 538);
+            this.playbox.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 43);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "make it";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtsize
+            // 
+            this.txtsize.Location = new System.Drawing.Point(163, 11);
+            this.txtsize.Name = "txtsize";
+            this.txtsize.Size = new System.Drawing.Size(46, 22);
+            this.txtsize.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "enter size of game box";
+            // 
+            // PuzzleGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 581);
+            this.Controls.Add(this.playbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtsize);
+            this.Controls.Add(this.label1);
+            this.Name = "PuzzleGame";
+            this.Text = "PuzzleGame";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtsize;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PuzzleGame.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment1
+{
+    public partial class PuzzleGame : Form
+    {
+        public PuzzleGame()
+        {
+            InitializeComponent();
+        }
+        Button[,] btns;
+        int size;
+        Boolean endgame;
+        int blankx,blanky,blanktag;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtsize.Text != "" && Int32.Parse(txtsize.Text) > 2)
+            {
+                size = Int32.Parse(txtsize.Text);
+                int[] cn = new int[size * size];
+                Random rand = new Random();
+                for(int i = 0;i < (size * size);i++)
+                {
+                    int a = rand.Next(1, (size * size)+1);
+                    if (i == 0)
+                    {
+                        cn[i] = a;
+                    }
+                    else
+                    {
+                        bool flag = true;
+                        while (flag)
+                        {
+                            int chk = 0;
+                            foreach (int c in cn)
+                            {
+                                if (c == a)
+                                {
+                                    a = rand.Next(1, (size * size)+1);
+                                    chk = 1;
+                                }
+                            }
+                            if (chk == 0)
+                            {
+                                flag = false;
+                                cn[i] = a;
+                            }
+                        }
+                    }
+                }
+                button1.Text = "Re Match";
+                playbox.Controls.Clear();
+                endgame = false;
+                int locx = 0;
+                int locy = 0;
+                btns = new Button[size, size];
+                int k = 0;
+                for (int i = 0; i < size; i++)
+                {
+                    locy = locy + 50;
+                    for (int j = 0; j < size; j++)
+                    {
+                        locx = locx + 50;
+                        btns[i, j] = new Button();
+                        btns[i, j].Width = 50;
+                        btns[i, j].Height = 50;
+                        btns[i, j].Location = new Point(locx, locy);
+                        btns[i, j].Name = (k+1).ToString();
+                        btns[i, j].Tag = k+1;
+                        if (cn[k] == size*size)
+                        {
+                            btns[i, j].Text = "";
+                            blankx = i;
+                            blanky = j;
+                            blanktag = k+1;
+                        }
+                        else
+                        {
+                            btns[i, j].Text = cn[k].ToString();
+                        }
+                        playbox.Controls.Add(btns[i, j]);
+                        btns[i, j].Click += new EventHandler(checkrun);
+                        k++;
+                    }
+                    locx = 0;
+                }
+            }
+        }
+        private void checkrun(object sender, EventArgs e)
+        {
+            if (endgame == false)
+            {
+                int x = 0,y = 0,tag = 0;
+                int btnadd = Int32.Parse((sender as Button).Name);
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (btns[i, j] == (sender as Button))
+                        {
+                            x = i;
+                            y = j;
+                            tag = btnadd;
+                        }
+                    }
+                }
+                if (btnadd - size == blanktag || btnadd + size == blanktag || (btnadd - 1 == blanktag && blankx == x) || (btnadd + 1 == blanktag && blankx == x))
+                {
+                    btns[blankx, blanky].Text = (sender as Button).Text;
+                    (sender as Button).Text = "";
+                    blankx = x;
+                    blanky = y;
+                    blanktag = tag;
+                    int cnt = 0;
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            if (btns[i, j].Text == btns[i, j].Name)
+                            {
+                                cnt++;
+                            }
+                            if (btns[size - 1, size - 1] == btns[i, j] && btns[i, j].Text == "")
+                            {
+                                cnt++;
+                            }
+                        }
+                    }
+                    if (cnt==(size*size))
+                    {
+                        MessageBox.Show("yeah you did it !");
+                        endgame = true;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
